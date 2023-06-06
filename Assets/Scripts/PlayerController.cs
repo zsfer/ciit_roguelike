794e8baf1bd3b58cfd04c3e7f371fd8b4d5d3a71
zsfer <seiferalbacete@gmail.com>
2023-06-06 15:11:57 +0800

    feat(sys): added PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D m_rb;
+    private Animator m_animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rb = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * 3, m_rb.velocity.y);
+        m_animator.SetFloat("Speed", Mathf.Abs(m_rb.velocity.x));
     }
 }
